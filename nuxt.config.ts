c19fa4e630aba484d1ef3446088d529c60dbc827
Vladimir_Belyaev <vogisa@mail.ru>
2024-01-22 13:53:56 +0300

    added some settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  imports: {
+    autoImport: false,
+  },
   modules: ["nuxt-primevue"],
   primevue: {
     components: {
+      prefix: "V",
       include: "*",
       exclude: [],
     },
