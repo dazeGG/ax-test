6df63bd29b68426d2af98488a64b5e01e9a9bdf3
Vladimir_Belyaev <vogisa@mail.ru>
2024-01-22 13:28:39 +0300

    added primevue and eslint + prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-})
+  modules: ["nuxt-primevue"],
+  primevue: {
+    components: {
+      include: "*",
+      exclude: [],
+    },
+  },
+});
