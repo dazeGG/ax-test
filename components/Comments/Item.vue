1b9af695024eeae2ebc74becddc7d4a3c7a3cce3
Vladimir_Belyaev <vogisa@mail.ru>
2024-01-22 18:12:39 +0300

    increased gap to comments list

--- components/Comments/Item.vue
+++ components/Comments/Item.vue
@@ -7,7 +7,32 @@ const props = defineProps<{
 </script>
 
 <template>
-  <span>{{ props.comment }}</span>
+  <div class="comment">
+    <div class="comment__user user">
+      <span class="user__name" v-text="props.comment.name" />
+      (<a
+        class="user__email"
+        :href="`mailto:${props.comment.email}`"
+        v-text="props.comment.email"
+      />)
+    </div>
+    <p class="comment__body" v-text="props.comment.body" />
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.comment {
+  &__body {
+    margin-top: 0.25rem;
+    font-size: 0.75rem;
+    font-weight: 100;
+  }
+}
+
+.user {
+  &__email {
+    font-weight: 100;
+    font-style: italic;
+  }
+}
+</style>
