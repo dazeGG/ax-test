2ba39490e3c6a0ecee4b5cc557f9f1369032c1fc
Vladimir_Belyaev <vogisa@mail.ru>
2024-01-22 13:57:18 +0300

    added List and Item to vue multi-word component names ignore

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,5 +10,9 @@ module.exports = {
   },
   extends: ["@nuxtjs/eslint-config-typescript", "plugin:prettier/recommended"],
   plugins: [],
-  rules: {},
+  rules: {
+    "vue/multi-word-component-names": ["error", {
+      "ignores": ["List", "Item"]
+    }]
+  },
 };
